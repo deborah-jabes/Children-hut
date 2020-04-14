bd103645aaf89723834b393a169adce225b70a1e
deborah-jabes <deborah.jabes@etu.univ-amu.fr>
2020-04-14 12:09:00 +0200

    ca marche pas mais y'a des trucs

---
+++ Message.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Message</title>
+		<?php
+		header ('Location: pageMsgEssai.php');?>
+		</head>
+	<body>
+	<p>C'est super</p>
+		<?php
+		echo "$apple_color";?>
+	</body>
+</html>
+
---
+++ pageMsgEssai.php
@@ -0,0 +1,13 @@
+<?php
+/*function headerFunc ($title) {
+	echo '<!DOCTYPE html>
+	<html>
+	<head>
+	<title>' . $title . '</title>' . PHP_EOL;
+	</head>
+	<body>' . PHP_EOL;
+}
+
+headerFunc ('page Message'); */
+
+$apple_color = 'blue';
