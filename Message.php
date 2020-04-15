ff34ffa05507e12d6d328ad6ae01bae28ee77e0c
deborah-jabes <deborah.jabes@etu.univ-amu.fr>
2020-04-15 11:51:21 +0200

    commit avec sublime merge

--- Message.php
+++ Message.php
@@ -1,5 +1,26 @@
-<?php 
-	header ('Location: pageMsgEssai.php');
+<?php
+function headerFunc ($titre) {
+	echo '<!DOCTYPE html>
+	<html lang="fr">
+	<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="currentStyle.css">
+	<title>' . $titre . '</title>
+	</head>
+	<body>';
+}
 
-
-
+headerFunc ('Message');
+ 	
+echo '
+<div>
+	<p>CHILDREN\'S HUT</p>
+	<ul id="menu">
+		<li><a href="index.html">HOME</a></li>
+		<li><a href="#buy">BUY</a></li>
+		<li><a href="#rent">RENT</a></li>
+		<li><a href="#sell">SELL</a></li>
+		<li><a href="#contact">CONTACT US</a></li>
+	</ul>
+</div>
+';
