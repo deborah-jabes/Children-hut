5ea2c13ef020ed778e7da0ff617c3304c8109abf
deborah-jabes <deborah.jabes@etu.univ-amu.fr>
2020-04-21 10:02:08 +0200

    backup du 2104

--- Message.php
+++ Message.php
@@ -5,59 +5,37 @@ function headerFunc ($titre) {
 	<head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="currentStyle.css">
-	<script src="messageJS"></script>
+	<link rel="stylesheet" type="text/css" href="assets/main.css">
 	<title>' . $titre . '</title>
 	</head>
 	<body>';
 }
 
 headerFunc ('Message');
- 	
+include("header.php");	
+
 echo '
-<div class="flex_container">
-	<div class="titre_gauche">
-		CHILDREN\'S HUTS
-	</div>
-
-	<div class="titrePage">
-	MESSAGE
-	</div>
-
-	<div class="menu_droite">
-		<ul id="menu">
-			<li><a href="index.html">HOME</a></li>
-			<li><a href="#buy">BUY</a></li>
-			<li><a href="#rent">RENT</a></li>
-			<li><a href="#sell">SELL</a></li>
-			<li><a href="#contact">CONTACT US</a></li>
-
-		</ul>
-	</div>
-</div>
-
-<div class="withBackgroundImg">
+	<img class="headimage" src="images/head.jpg" alt="Head image">
 	<div class="valuesMessage" id="valuesMessage">
 
-		<div class="valuesToEnter">
-			<form id="formulaireMessage" action="">';//that is to choose later on 
+			<div class="valuesToEnter">
+				<form id="formulaireMessage" action="">'//that is to choose later on 
+				.'<input type="text" id="name" name="name" placeholder="Name"></br></br>
+				<input type="text" id="mailAdr" name="mail" placeholder="e-mail address"></br></br>
+				<select id="Objects" onclick="disable()">
+					<option id="clickToSelect" >Click to select</option>
+					<option value="buy">Buy a hut </option>
+					<option value="rent">Rent a hut </option>
+					<option value="details">Ask for details </option>
+					<option value="other">Other</option>
+				</select></br></br>
 			
-			echo 
-			'
-			<input type="text" id="name" name="name" placeholder="Name"></br></br>
-			<input type="text" id="mailAdr" name="mail" placeholder="e-mail address"></br></br>
-			<select id="Objects">
-				<option onclick="disable()">Click to select</option>
-				<option value="buy">Buy a hut </option>
-				<option value="rent">Rent a hut </option>
-				<option value="details">Ask for details </option>
-				<option value="other">Other</option>
-			</select></br></br>
-		</div>
 
 		<textarea id="message" placeholder="Your message here" rows="10" cols="70" maxlength="1000"></textarea></br></br>
-		<input type="submit" value="submit" id="submit" onclick="replace()">
-		</form>
-	</div>
-</div>		
+		<input type="submit" value="submit" id="submit" onclick="replaceAndCreate()"><br><br>
+		 </form></div>
 
-';
+		 <script src="messageJS.js"></script>
+		 </body>';
+
+include ("footer.php");	
