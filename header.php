338ec7d93661f8f0decab5036185f9a85af4a0ba
nheudron <samdroid@gmx.com>
2020-04-26 19:41:37 +0200

    J'ai commencé le menu mobile.

--- header.php
+++ header.php
@@ -8,13 +8,9 @@
 	<a href="#menu" class="menuToggle"><img src="images/menu.svg" alt="menu"></a>
 	<nav id="nav" class="menu">
 		<ul id="navbar" class="navbar">
-			<li><a href="index.php" class="fa-"> Home</a></li>
-			<li><a href=".php" class="fa-"> Buy</a></li>
-			<li><a href=".php" class="fa-"> Rent</a></li>
-			<li><a href=".php"class="fa-"> Sale</a></li>
-			<li><a href="#contact" class="scrolly">Contact us</a></li>
-			<li><a href="login.php"><img class="account" src="images/account.png"></a></li>
-			<figure class="circle"></figure>
+			<li><a href=".php">Buy</a></li><li><a href=".php" >Rent</a></li><li><a href=".php">Sale</a></li><li><a href="#contact">Contact us</a></li><li><a href="login.php" class="account"><img src="images/account.png"></a></li>
+			
 		</ul>
-	</nav>	
+	</nav>
+	<figure class="circle"></figure>
 </header>
