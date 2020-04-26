7416e3710b1e691fdfb4132a13ba3b4f33367c19
nheudron <samdroid@gmx.com>
2020-04-26 13:56:42 +0200

    ajout d'une balise head dans header.php pour mettre le favicon sur toutes les pages et mettre le pack d'icon fontawesome

--- header.php
+++ header.php
@@ -1,13 +1,18 @@
+<head>
+	<link rel="icon" href="images/favicon.png" />
+	<script src="https://kit.fontawesome.com/10a40eb87c.js" crossorigin="anonymous"></script>
+</head>
+
 <header id="header">
 		<h1><a  href="index.html">CHILDREN'S HUT</a></h1>
 		<nav id="nav">	
 			<ul id="navbar">
 							<li><a href="index.php" class="fa-"> Home</a></li>
-							<li><a href=".html" class="fa-"> Buy</a></li>
-							<li><a href=".html" class="fa-"> Rent</a></li>
-							<li><a href=".html"class="fa-"> Sale</a></li>
+							<li><a href=".php" class="fa-"> Buy</a></li>
+							<li><a href=".php" class="fa-"> Rent</a></li>
+							<li><a href=".php"class="fa-"> Sale</a></li>
 							<li><a href="#contact" class="scrolly">Contact us</a></li>
-							<li><a href="index.html"><img class="account" src="images/account.png"></a></li>
+							<li><a href="login.php"><img class="account" src="images/account.png"></a></li>
 
 			</ul>
 			<ul>
