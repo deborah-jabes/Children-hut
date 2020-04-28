c46ec48638bf499d8ea398b3f04913e4e0c22c74
deborah-jabes <deborah.jabes@etu.univ-amu.fr>
2020-04-28 11:08:08 +0200

    mailIncoming

--- messageJS.js
+++ messageJS.js
@@ -22,47 +22,11 @@ function replaceAndCreate () {
 	formulaireMessage.appendChild(newButton);
 
 	//sending the email 
-	/*let email = document.getElementById ("mailAdr").value;
+	let email = document.getElementById ("mailAdr").value;
 	let subject = document.getElementById ("Objects").value;
 	let message = document.getElementById ("message").value;
-	window.location.href = "mailto:" + email 
-	+ "?subject=" + escape (subject) 
-	+ "&body=" + escape (message);*/
+	window.location.href = "mailto:" + email + "?subject=" + escape (subject) + "&body=" + escape (message);
 
-	//server based so can't call it from client-sided js
-	<?php
-
-		echo 'salut';
-
-		$to = $_POST['mail'];
-		$name = $_POST['name'];
-
-		$subject = $_POST['object'];
-		$body = $_POST['msg'];
-
-		$mailer = new PHPMailer();
-
-		//Settings
-		$mailer->IsSMTP();
-		$mail->CharSet = 'UTF-8';
-
-		$mail->Host       = "smtp-locagarden.alwaysdata.net"; // SMTP server 
-		$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
-		$mail->SMTPAuth   = true;                  // enable SMTP authentication
-		$mail->Port       = 25;                    // set the SMTP port to default
-		$mail->Username   = "locagarden"; // SMTP account username
-		$mail->Password   = "Loc@Garden1404";        // SMTP account password
-
-		// Content
-		$mail->isHTML(true);                                  // Set email format to HTML
-		$mail->Subject = $subject;
-		$mail->Body    = $body;
-		$mail->AltBody = 'hello ' + $name;
-
-		$mail->send();
-		mail ($to, $subject, $message);
-
-	?>
 }
 
 function disable () {
