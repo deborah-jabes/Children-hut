6e8aec89d5dc53992a73fa8f4ba30c2a640a92fd
nheudron <samdroid@gmx.com>
2020-04-30 16:09:20 +0200

    créa pages buy et rent

---
+++ buy.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Children's hut</title>
+	<link rel="icon" href="images/favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link href="css/main.css" rel="stylesheet" type="text/css">
+	<script src="https://kit.fontawesome.com/10a40eb87c.js" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> <!--JQuery-->
+</head>
+<body id="body">
+	
+	<?php include("header.php")?>
+		<section class="space"></section>
+		
+		<div class="research">  
+				<form action=""> 
+					<button type="submit" class="btn btn-success">
+						<i class="fa fa-search fa-1x"></i>
+					</button>
+					<input type="search" placeholder="Research"></input>
+				</form>
+		</div>
+
+		<section class="announcements">
+			<div class="center">
+				<h2>Recent Announcements</h2>
+				<TABLE>
+					<TR>
+						<TD>
+							<div>
+								<img src="images/picAnnouncement.png">
+								<header>
+									<h4>Announcement</h4>
+									<button class="heart"><img src="images/heart.svg"></button>
+								</header>
+								<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu  </p>
+								<footer class="flexSection">
+									<section><p class="price">25€</p></section>
+									<section>
+										<button type="button">Buy</button>
+										<button>Details</button>
+									</section>
+								</footer>
+							</div>
+						</TD>
+						<TD>
+							<div>
+								<img src="images/picAnnouncement.png">
+								<header>
+									<h4>Announcement</h4>
+									<button class="heart"><img src="images/heart.svg"></button>
+								</header>
+								<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu  </p>
+								<footer class="flexSection">
+									<section><p class="price">25€</p></section>
+									<section>
+										<button type="button">Buy</button>
+										<button>Details</button>
+									</section>
+								</footer>
+							</div>
+						</TD>
+						<TD>
+							<div>
+								<img src="images/picAnnouncement.png">
+								<header>
+									<h4>Announcement</h4>
+									<button class="heart"><img src="images/heart.svg"></button>
+								</header>
+								<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu  </p>
+								<footer class="flexSection">
+									<section><p class="price">25€</p></section>
+									<section>
+										<button type="button">Buy</button>
+										<button>Details</button>
+									</section>
+								</footer>
+							</div>
+						</TD>
+						<TD>
+							<div>
+								<img src="images/picAnnouncement.png">
+								<header>
+									<h4>Announcement</h4>
+									<button class="heart"><img src="images/heart.svg"></button>
+								</header>
+								<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu  </p>
+								<footer class="flexSection">
+									<section><p class="price">25€</p></section>
+									<section>
+										<button type="button">Buy</button>
+										<button>Details</button>
+									</section>
+								</footer>
+							</div>
+						</TD>
+						<TD>
+							<div>
+								<img src="images/picAnnouncement.png">
+								<header>
+									<h4>Announcement</h4>
+									<button class="heart"><img src="images/heart.svg"></button>
+								</header>
+								<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu  </p>
+								<footer class="flexSection">
+									<section><p class="price">25€</p></section>
+									<section>
+										<button type="button">Buy</button>
+										<button>Details</button>
+									</section>
+								</footer>
+							</div>
+						</TD>
+						<TD>
+							<div>
+								<img src="images/picAnnouncement.png">
+								<header>
+									<h4>Announcement</h4>
+									<button class="heart"><img src="images/heart.svg"></button>
+								</header>
+								<p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu  </p>
+								<footer class="flexSection">
+									<section><p class="price">25€</p></section>
+									<section>
+										<button type="button">Buy</button>
+										<button>Details</button>
+									</section>
+								</footer>
+							</div>
+						</TD>
+					</TR>
+				</TABLE>
+				<div>
+
+				</div>
+			</div>
+		</section>
+
+		<?php include("footer.php"); ?>
+
+	<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="e82eeddb-4dac-4972-9a28-304a54f8e032";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+</body>
+</html>
