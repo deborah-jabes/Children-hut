1caa11d32a637472122be9559cb8a5f313621f65
deborah-jabes <deborah.jabes@etu.univ-amu.fr>
2020-04-30 16:29:09 +0200

    beginning classified ad page

--- Message.php
+++ Message.php
@@ -3,7 +3,6 @@ include ('usefulFunctions.php');
 
 headerFunc ('Message');
 include("header.php");	
-$emailValue = $_POST['mail'];
 session_start();
 
 echo '<img class="headimage" src="images/head.jpg" alt="Head image">
@@ -11,7 +10,7 @@ echo '<img class="headimage" src="images/head.jpg" alt="Head image">
 
 			<div class="valuesToEnter">
 				<form id="formulaireMessage" onsubmit="replaceAndCreate(); return false" method="post">
-				<h2>MESSAGE</h2>
+				<h2 id="messageTitle">MESSAGE</h2>
 				<input type="text" id="name" name="name" required placeholder="Name"></br></br>
 				<input type="text" id="mailAdr" name="mail" required placeholder="e-mail address"></br></br>
 				<select id="Objects" name="object" onclick="disable()" required> 
