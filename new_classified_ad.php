3c95c245dbd5e9575726cbabc438514d4df1360c
Chataigner Firmin <45859894+FifiElPapa@users.noreply.github.com>
2020-05-06 22:17:46 +0200

    new classified add css fini
    Htlm et css fini

--- new_classified_ad.php
+++ new_classified_ad.php
@@ -11,66 +11,70 @@
 	<?php include("header.php");
 		include("db_connection.php");
 		session_start();
+		//https://developer.mozilla.org/fr/docs/Web/API/FileReader/readAsDataURL
 	?>
 
 	<section class="newClassifiedAd">
 		<h1>Create a new classified ad</h1>
 		<div class="divForm">
-			<form method="post" action="new_classified_adC.php" enctype="multipart/form-data">
-				<table>
-					<tr>
-						<td>
-							<img src="images/hut.png" id="preview1">
-						</td>
-						<td>
-							<img src="images/hut.png" id="preview2">
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<input type="file" name="fileToUpload1" id="fileToUpload1">
-						</td>
-						<td>
-							<input type="file" name="fileToUpload2" id="fileToUpload2">
-						</td>
-					</tr>
-				</table>
-				<br>
-				<input type="text" name="Title" id="Title" placeholder="Name" maxlength="50" required>
-				<br>
-				<input type="text" name="Material" id="Material" placeholder="Main Material" maxlength="20" required>
-				<br>
-				<input type="text" name="Color" id="Color" placeholder="Main Color" maxlength="20" required>
-				<br>
-				<input type="text" name="WindowsNB" id="WindowsNB" placeholder="Number of windows" maxlength="2">
-				<br>
-				<input type="checkbox" name="Slide" id="Slide">
-				<label for="Slide">Slide</label>
-				<br>
-				<input type="text" name="FloorsNB" id="FloorsNB" placeholder="Number of floors" maxlength="2">
-				<br>
-				<input type="text" name="SurfaceArea" id="SurfaceArea" placeholder="Surface area in m3" maxlength="3">
-				<br>
-				<input type="text" name="CeilingHeight" id="CeilingHeight" placeholder="Ceiling Height" maxlength="4">
-				<br>
-				<input type="checkbox" name="Terrace" id="Terrace">
-				<label for="Terrace">Terrace</label>
-				<br>
-				<input type="checkbox" name="Wheels" id="Wheels">
-				<label for="Wheels">Wheels</label>
-				<br>
-				<input type="text" name="Price" id="Price" placeholder="Price" maxlength="6" required>
-				<br>
-				<input type="text" name="Country" id="Country" placeholder="Country" maxlength="30" required>
-				<br>
-				<input type="text" name="Description" id="Description" placeholder="Description" maxlength="10000">
-				<br>
-				<input type="radio" name="SellRent" id="Sell" value="sell" checked>
-				<label for="Sell">Sell</label>
-				<br>
-				<input type="radio" name="SellRent" id="Rent" value="rent">
-				<label for="Rent">Rent</label>
-				<br>
+			<form method="post" action="new_classified_adC.php" enctype="multipart/form-data" autocomplete="off">
+				<div class="imagePreview">
+					<div class="addpading">
+						<img src="images/hut.png" id="preview1" class="previewimg">
+						<br>
+						<input type="file" name="fileToUpload1" id="fileToUpload1">
+					</div>
+					<div class="addpading">
+						<img src="images/hut.png" id="preview2" class="previewimg">
+						<br>
+						<input type="file" name="fileToUpload2" id="fileToUpload2">
+					</div>
+				</div>
+
+				<div class="newAdSpecs">
+					<div>
+						<input type="text" name="Title" id="Title" placeholder="Name" maxlength="50" required>
+						<br>
+						<input type="text" name="Material" id="Material" placeholder="Main Material" maxlength="20" required>
+						<br>
+						<input type="text" name="Color" id="Color" placeholder="Main Color" maxlength="20" required>
+						<br>
+						<input type="text" name="WindowsNB" id="WindowsNB" placeholder="Number of windows" maxlength="2">
+						<br>
+						<input type="checkbox" name="Slide" id="Slide">
+						<label for="Slide">Slide</label>
+						<br>
+						<input type="text" name="FloorsNB" id="FloorsNB" placeholder="Number of floors" maxlength="2">
+						<br>
+						<input type="text" name="SurfaceArea" id="SurfaceArea" placeholder="Surface area in m3" maxlength="3">
+						<br>
+						<input type="text" name="CeilingHeight" id="CeilingHeight" placeholder="Ceiling Height" maxlength="4">
+						<br>
+						<input type="checkbox" name="Terrace" id="Terrace">
+						<label for="Terrace">Terrace</label>
+						<br>
+						<input type="checkbox" name="Wheels" id="Wheels">
+						<label for="Wheels">Wheels</label>
+					</div>
+					<div class="Rinfodiv">
+						<input type="text" name="Price" id="Price" placeholder="Price" maxlength="6" required>
+						<br>
+						<input type="text" name="Country" id="Country" placeholder="Country" maxlength="30" required>
+						<br>
+						<textarea name="Description" id="Description" placeholder="Description" maxlength="10000" rows="7"></textarea>
+						<br>
+						<div class="sellrentdiv">
+							<div>
+								<input type="radio" name="SellRent" id="Sell" value="sell" checked>
+								<label for="Sell">Sell</label>
+							</div>
+							<div>
+								<input type="radio" name="SellRent" id="Rent" value="rent">
+								<label for="Rent">Rent</label>
+							</div>
+						</div>
+					</div>
+				</div>
 				<input type="submit" name="submit" value="Publish">
 			</form>
 		</div>
