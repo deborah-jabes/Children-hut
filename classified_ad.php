2ba75ac2f9bb009ccb127d30e0ce80df04540aa3
deborah-jabes <deborah.jabes@etu.univ-amu.fr>
2020-05-07 12:23:12 +0200

    classified ad page finished git add *

--- classified_ad.php
+++ classified_ad.php
@@ -20,15 +20,38 @@ $columnName = $db->prepare('SELECT * from huts where Hut_id = ?');
 $columnName->execute(array('1'));
 
 	foreach ($columnName->fetch(PDO::FETCH_ASSOC) as $key => $value) {
-
-		if ($value == 0) {
-			PHP_EOL;
-		}
-		else {
-			echo '<li><b>' . str_replace('_', ' ', $key) . '</b> : '. $value . '</li>';	
-		}
-
 		
+		
+
+		switch (true) {
+			case $key == 'Author_id' || $key == 'Description' || $key == 'Hut_id' || $key == 'Pictures_path':
+				echo '';
+				break;
+
+			case $key == 'Surface_area':
+				echo '<li><b>' . str_replace('_', ' ', $key) . '</b> : '. $value . ' m²</li>';
+				break;
+
+			case $key == 'Price':
+				echo '<li><b>' . str_replace('_', ' ', $key) . '</b> : '. $value . ' €</li>';
+				break;
+
+			case $key == "Ceiling_height":
+				echo '<li><b>' . str_replace('_', ' ', $key) . '</b> : '. $value . ' m</li>';
+				break;
+
+			case $value == '0':
+				echo '<li class="contentWithCross"><b>' . str_replace('_', ' ', $key) . '</b> : '. 'No' . '</li>'; 
+				break;
+
+			case $value == '1':
+				echo '<li><b>' . str_replace('_', ' ', $key) . '</b> : '. 'Yes' . '</li>';
+				break;
+
+			default: 
+				echo '<li><b>' . str_replace('_', ' ', $key) . '</b> : '. $value . '</li>';
+				break;
+		}
 		
 	}
 }
@@ -45,7 +68,6 @@ while ($data = $titles->fetch()) {
 	else {
 		$rentOrBuy = 'BUY';
 	}
-	//<li>Element1</li><li>Element2</li><li>Element3</li>
 
 
 
@@ -53,6 +75,10 @@ while ($data = $titles->fetch()) {
 	<img class="headimage" src="images/head.jpg" alt="Head image">
 	<div class="container">
 		<h2 id="hutsName">HUT '. $data["Title"] .'</h2>
+		<div id="country">
+		<img id="img_localisation" src="/images/localisation.png" alt="localisation_img"></img>
+		<a>'.$data["Town"].'<a>
+		</div>
 
 	<div class="flexContainer">
 		<div class="imgAndSpecsContainer">
@@ -72,7 +98,7 @@ while ($data = $titles->fetch()) {
 
 		<div class="description">
 			<p>DESCRIPTION</p>
-			<p class="descriptionContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><br>
+			<p class="descriptionContent">'. $data["Description"] .'</p><br>
 			<p>THE SELLER</p>
 			<p class="descriptionContent"><a href="login.php">Log in</a> to see more information</p>
 		</div>
