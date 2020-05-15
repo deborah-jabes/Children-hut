f3a861ed3fb9109bbf8c3c1ed8c2799845aa4082
Chataigner Firmin <45859894+FifiElPapa@users.noreply.github.com>
2020-05-15 11:31:41 +0200

    Update addToFav.php

--- addToFav.php
+++ addToFav.php
@@ -5,11 +5,17 @@
 	if (isset($_SESSION["Email_address"])) {
 		$hut = intval($_POST['Hut_id']);
 		$user = $_SESSION["User_id"];
-		$result = $db->prepare('INSERT INTO favorites(User_id, Hut_id) VALUES (:user, :hut)');
-		$result->execute(array(
-			'user'=>$user,
-			'hut'=>$hut
-		));
+
+		$result = $db->prepare('SELECT * FROM favorites WHERE User_id = :user AND Hut_id = :hut;');
+		$result->execute(array('user'=>$user,'hut'=>$hut));
+
+		if ($result->rowCount() == 0) {
+			$result = $db->prepare('INSERT INTO favorites(User_id, Hut_id) VALUES (:user, :hut)');
+			$result->execute(array(
+				'user'=>$user,
+				'hut'=>$hut
+			));
+		}
 	}else{
 		echo "<script>window.top.location.href = \"login.php\";</script>";
 	}
